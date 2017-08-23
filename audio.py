0b21838e6dc050f3f7f9f2668c137f1d327ea309
amylashley <amylashley@gmail.com>
2017-08-23 19:22:23 -0400

    adding new color ramps

--- audio.py
+++ audio.py
@@ -4,21 +4,32 @@ from matplotlib.mlab import find
 import math
 import serial
 
+
 CHUNK = 2**11
 RATE = 44100
 MAX_AMP = 16000.0 # <- Adjust this based on input level
 MAX_FREQ = 10000.0
-PORT = '/dev/cu.usbmodem27'
+PORT = '/dev/cu.usbmodem100'
 
-# Not used currently, may be used to tell Arduino to update whole string
-# rather than moving each pixel to the next LED.
-mode = 0
+# Within each mode type, we can define many color ramps
+# We will reference these by the index of the ramp
+ramp_index = 0
 
-# switch between amplitude and frequency modes
-amp_mode = 1
+# switch between amplitude, frequency, and random modes
+# amplitude = 0 frequency = 1 random = 2
+mode_type = 1
 
-color_ramp = [((0, 0, 0), (0, 0, 0), (54, 2, 2), (90, 0, 0), (126, 0, 0), (165, 0, 0), (209, 0, 0), (237, 0, 0), (255, 0, 0), (255, 35, 35), (255, 76, 76), (255, 115, 115), (255, 148, 148), (255, 187, 187), (255, 226, 226)),
-((0, 0, 0), (0, 0, 0), (2, 54, 2), (0, 90, 0), (0, 126, 0), (0, 165, 0), (0, 209, 0), (0, 237, 0), (0, 237, 0), (35, 255, 35), (76, 255, 76), (115, 255, 115), (148, 255, 148), (187, 255, 187), (226, 255, 226))]
+amp_color_ramps = [((0, 0, 0), (0, 0, 0), (54, 2, 2), (90, 0, 0), (126, 0, 0), (165, 0, 0), (209, 0, 0), (237, 0, 0), (255, 0, 0), (255, 35, 35), (255, 76, 76), (255, 115, 115), (255, 148, 148), (255, 187, 187), (255, 226, 226)),
+((0, 0, 0), (0, 0, 0), (2, 54, 2), (0, 90, 0), (0, 126, 0), (0, 165, 0), (0, 209, 0), (0, 237, 0), (0, 237, 0), (35, 255, 35), (76, 255, 76), (115, 255, 115), (148, 255, 148), (187, 255, 187), (226, 255, 226)), 
+((80, 233, 246), (106, 198, 243), (134, 162, 238) , (165, 119, 234), (196, 79, 230), (225, 40, 226) , (248, 10, 223)),
+((80, 233, 246), (76, 234, 232), (65, 237, 199) , (52, 240, 159), (39, 244, 118), (24, 248, 75) , (12, 251, 37), (3, 254, 9)),
+((255, 0, 28), (255, 0, 253), (141, 0, 255) , (0, 26, 255), (0, 255, 238), (0, 255, 33) , (161, 255, 0), (255, 225, 0), (255, 95, 0)),
+((0, 255, 2), (0, 198, 94), (8, 84, 255) , (112, 25, 255), (159, 7, 255), (158, 7, 255) ),
+((246, 249, 3), (247, 226, 3), (249, 192, 0) , (250, 152, 1), (252, 114, 1), (253, 75, 1) , (254, 44, 0), (255, 24, 0)),
+((1, 251, 255), (53, 255, 189), (198, 255, 21) , (250, 244, 0), (255, 158, 72), (255, 69, 161) , (255, 3, 227))]
+freq_color_ramps = []
+
+random_color_ramps = []
 
 # More colors needed here...
 freq_colors = [(255,0,0),(0,255,0),(0,0,255)]
@@ -39,34 +50,34 @@ def Pitch(signal):
 # used to switch between color ramps
 # how often to switch - maybe this should be done with the Makey Makey controller?
 counter = 0
-i = 0 # index of the color_ramp array
+i = 5 # index of the color_ramp array
 
 while True:
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-    if amp_mode:
-        peak=np.average(np.abs(data))*2
-        amp = peak/MAX_AMP
-        print peak
-        num_colors = len(color_ramp[i])
-        for j in range(num_colors):
-            if amp > j/(float(num_colors)):
-                color = color_ramp[i][j]
-    else:
-        Frequency=Pitch(data)
-        print "%f Frequency" %Frequency
-        num_colors = len(freq_colors)
-        for j in range(num_colors):
-            if Frequency/MAX_FREQ > j/(float(num_colors)):
-                color = freq_colors[j]
+    #if amp_mode:
+    peak=np.average(np.abs(data))*2
+    amp = peak/MAX_AMP
+    #print peak
+    num_colors = len(amp_color_ramps[i])
+    for j in range(num_colors):
+        if amp > j/(float(num_colors)):
+            color = amp_color_ramps[i][j]
+    #else:
+    #    Frequency=Pitch(data)
+    #    print "%f Frequency" %Frequency
+    #    num_colors = len(freq_colors)
+    #    for j in range(num_colors):
+    #        if Frequency/MAX_FREQ > j/(float(num_colors)):
+    #            color = freq_colors[j]
 
     # Arduino expects mode, R, G, B over Serial
-    print("{0},{1},{2},{3}".format(mode, color[0], color[1], color[2]))
-    ser.write("{0},{1},{2},{3},".format(mode, color[0], color[1], color[2]))
+    print("{0},{1},{2},{3}".format(mode_type, color[0], color[1], color[2]))
+    ser.write("{0},{1},{2},{3},".format(mode_type, color[0], color[1], color[2]))
 
-    counter += 1
-    if (counter > 100):
-        i = (i + 1) % 2
-        counter = 0
+    #counter += 1
+    #if (counter > 100):
+    #    i = (i + 1) % 2
+    #    counter = 0
 
 
 ser.close()
