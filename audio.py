bbbf7c83f515a1b00c324438738e9a54a9600f8c
amylashley <amylashley@gmail.com>
2017-09-04 13:19:54 -0400

    adding in the work from last week: use only one color_ramps array for all modes; setting for amp vs freq; mode type for running/wholestring/image

--- audio.py
+++ audio.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib.mlab import find
 import math
 import serial
+import keyboard
 
 
 CHUNK = 2**11
@@ -11,17 +12,31 @@ MAX_AMP = 16000.0 # <- Adjust this based on input level
 MAX_FREQ = 10000.0
 PORT = '/dev/cu.usbmodem1411'
 
-# Within each mode type, we can define many color ramps
-# We will reference these by the index of the ramp
-ramp_index = 0
+# arrow key codes
+left_arrow  = 37
+up_arrow    = 38
+right_arrow = 39
+down_arrow  = 40
 
-# switch between amplitude, frequency, and random modes
-# amplitude = 0 frequency = 1 random = 2
-mode_type = 0
+# amp vs freq will be hard coded for now
+# 0 = amp 1 = freq
+amp_freq = 1;
+
+# switch between running, whole string, image
+# controlled with the Makey Makey controller
+# running = 0, whole string = 1, image = 2
+mode_type = 0  #always default to amplitude
+
+# controlled with the Makey Makey controller
+# if we have n numbers of ramps in a mode, then if mod(n+1) = 0 then we can change modes
+# current index inside the current color_ramps[mode_type] array
+ramp_index = 0 
+
 
 pins = [0]
 
-amp_color_ramps = [((0, 0, 0), (0, 0, 0), (54, 2, 2), (90, 0, 0), (126, 0, 0), (165, 0, 0), (209, 0, 0), (237, 0, 0), (255, 0, 0), (255, 35, 35), (255, 76, 76), (255, 115, 115), (255, 148, 148), (255, 187, 187), (255, 226, 226)),
+
+color_ramps = [((0, 0, 0), (0, 0, 0), (54, 2, 2), (90, 0, 0), (126, 0, 0), (165, 0, 0), (209, 0, 0), (237, 0, 0), (255, 0, 0), (255, 35, 35), (255, 76, 76), (255, 115, 115), (255, 148, 148), (255, 187, 187), (255, 226, 226)),
 ((0, 0, 0), (0, 0, 0), (2, 54, 2), (0, 90, 0), (0, 126, 0), (0, 165, 0), (0, 209, 0), (0, 237, 0), (0, 237, 0), (35, 255, 35), (76, 255, 76), (115, 255, 115), (148, 255, 148), (187, 255, 187), (226, 255, 226)), 
 ((80, 233, 246), (106, 198, 243), (134, 162, 238) , (165, 119, 234), (196, 79, 230), (225, 40, 226) , (248, 10, 223)),
 ((80, 233, 246), (76, 234, 232), (65, 237, 199) , (52, 240, 159), (39, 244, 118), (24, 248, 75) , (12, 251, 37), (3, 254, 9)),
@@ -29,12 +44,6 @@ amp_color_ramps = [((0, 0, 0), (0, 0, 0), (54, 2, 2), (90, 0, 0), (126, 0, 0), (
 ((0, 255, 2), (0, 198, 94), (8, 84, 255) , (112, 25, 255), (159, 7, 255), (158, 7, 255) ),
 ((246, 249, 3), (247, 226, 3), (249, 192, 0) , (250, 152, 1), (252, 114, 1), (253, 75, 1) , (254, 44, 0), (255, 24, 0)),
 ((1, 251, 255), (53, 255, 189), (198, 255, 21) , (250, 244, 0), (255, 158, 72), (255, 69, 161) , (255, 3, 227))]
-freq_color_ramps = []
-
-random_color_ramps = []
-
-# More colors needed here...
-freq_colors = [(255,0,0),(0,255,0),(0,0,255)]
 
 p=pyaudio.PyAudio()
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
@@ -49,13 +58,47 @@ def Pitch(signal):
     f0=round(len(index) *RATE /(2*np.prod(len(signal))))
     return f0;
 
-# used to switch between color ramps
-# how often to switch - maybe this should be done with the Makey Makey controller?
-counter = 0
-i = 5 # index of the color_ramp array
-
 while True:
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
+
+
+    #set the number of color ramps we have
+    print "ramp_index %i" %ramp_index
+    num_colors = len(color_ramps[ramp_index])
+    print "num_colors %i" %num_colors
+
+    #determine the mode and act accordingly
+    if amp_freq == 0:
+        # if keyboard.is_pressed(down_arrow): 
+        #     if (current_amp > RATE_OF_CHANGE): # whats the lowest we can go here
+        #         current_amp = current_amp - RATE_OF_CHANGE
+        # if keyboard.is_pressed(up_arrow): 
+        #     if (current_amp < MAX_AMP):
+        #         current_amp = current_amp + RATE_OF_CHANGE
+
+        #use the amplitude mode
+        peak=np.average(np.abs(data))*2
+        amp = peak/current_amp
+        #print peak
+        for j in range(num_colors):
+            if amp > j/(float(num_colors)):
+                color = color_ramps[ramp_index][j]
+    elif amp_freq == 1:
+        # if keyboard.is_pressed(down_arrow): 
+        #     if (current_freq > RATE_OF_CHANGE): #whats the lowest we can go here?
+        #         current_freq = current_freq - RATE_OF_CHANGE
+        # if keyboard.is_pressed(up_arrow): 
+        #     if (current_freq < MAX_FREQ):
+        #         current_freq = current_freq + RATE_OF_CHANGE
+
+        #use the frequency mode
+        Frequency=Pitch(data)
+        print "%f Frequency" %Frequency        
+        for j in range(num_colors):
+            if Frequency/current_freq > j/(float(num_colors)):
+                color = color_ramps[ramp_index][j]
+
+
     #if amp_mode:
     peak=np.average(np.abs(data))*2
     amp = peak/MAX_AMP
@@ -72,14 +115,25 @@ while True:
     #        if Frequency/MAX_FREQ > j/(float(num_colors)):
     #            color = freq_colors[j]
 
-    # Arduino expects mode, R, G, B over Serial
+    # Arduino expects pin, mode, R, G, B over Serial
     print("{0},{1},{2},{3},{4}".format(pins[0], mode_type, color[0], color[1], color[2]))
     ser.write("{0},{1},{2},{3},{4},".format(pins[0], mode_type, color[0], color[1], color[2]))
 
-    #counter += 1
-    #if (counter > 100):
-    #    i = (i + 1) % 2
-    #    counter = 0
+#testing loop should be replaced by keypress code
+    # counter += 1
+    # if (counter > 100):
+    #     if ramp_index == len(color_ramps) - 1:
+    #         #change the mode and reset the counter
+    #         ramp_index = 0;
+    #         # if (mode_type < 2):
+    #         #     mode_type+=1;
+    #         # else:
+    #         #     mode_type = 0;            
+    #     else:
+    #         #increase the counter inside the same mode
+    #         ramp_index+=1;
+    #     counter = 0
+
 
 
 ser.close()
