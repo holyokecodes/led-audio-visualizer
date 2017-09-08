6c60ffaaf4fbecec1ffd664027f3d858c9c45a94
Andrew Pasquale <andrew@elytra.net>
2017-09-08 08:27:58 -0400

    Add screensaver if it is quiet for a period.

--- audio.py
+++ audio.py
@@ -3,17 +3,25 @@ import numpy as np
 from matplotlib.mlab import find
 import math
 import serial
-import keyboard
+from time import time
+import random
+#import keyboard
 
 
-CHUNK = 2**12
+CHUNK = 2**11
 RATE = 44100
 MAX_AMP = 10000.0 # <- Adjust this based on input level
 MAX_FREQ = 10000.0
 RATE_OF_CHANGE = 500 # amount to adjust max values
-PORT_A = '/dev/ttyACM0'
-PORT_B = '/dev/ttyACM1'
+#PORT_A = '/dev/ttyACM0'
+#PORT_B = '/dev/ttyACM1'
+PORT_A = '/dev/cu.usbmodem56'
 
+# Screensaver settings
+NAP_TIME = 5 # Seconds before screensaver starts
+sleep_time = time()
+napping_color = []
+color = [random.randint(0,10), random.randint(0,10), random.randint(0,10)]
 
 # arrow key codes
 up_arrow    = 38
@@ -30,21 +38,21 @@ amp_freq = 0;
 # switch between running, whole string, image
 # controlled with the Makey Makey controller
 # running = 0, whole string = 1, image = 2
-mode_type = 0  #always default to amplitude
+mode_type = 1
 
 # controlled with the Makey Makey controller
 # if we have n numbers of ramps in a mode, then if mod(n+1) = 0 then we can change modes
 # current index inside the current color_ramps[mode_type] array
-ramp_index = 0
+ramp_index = 4
 
 color_ramps = [((0, 0, 0), (0, 0, 0), (54, 2, 2), (90, 0, 0), (126, 0, 0), (165, 0, 0), (209, 0, 0), (237, 0, 0), (255, 0, 0), (255, 35, 35), (255, 76, 76), (255, 115, 115), (255, 148, 148), (255, 187, 187), (255, 226, 226)),
-((0, 0, 0), (0, 0, 0), (2, 54, 2), (0, 90, 0), (0, 126, 0), (0, 165, 0), (0, 209, 0), (0, 237, 0), (0, 237, 0), (35, 255, 35), (76, 255, 76), (115, 255, 115), (148, 255, 148), (187, 255, 187), (226, 255, 226)), 
-((80, 233, 246), (106, 198, 243), (134, 162, 238) , (165, 119, 234), (196, 79, 230), (225, 40, 226) , (248, 10, 223)),
-((80, 233, 246), (76, 234, 232), (65, 237, 199) , (52, 240, 159), (39, 244, 118), (24, 248, 75) , (12, 251, 37), (3, 254, 9)),
-((255, 0, 28), (255, 0, 253), (141, 0, 255) , (0, 26, 255), (0, 255, 238), (0, 255, 33) , (161, 255, 0), (255, 225, 0), (255, 95, 0)),
-((0, 255, 2), (0, 198, 94), (8, 84, 255) , (112, 25, 255), (159, 7, 255), (158, 7, 255) ),
-((246, 249, 3), (247, 226, 3), (249, 192, 0) , (250, 152, 1), (252, 114, 1), (253, 75, 1) , (254, 44, 0), (255, 24, 0)),
-((1, 251, 255), (53, 255, 189), (198, 255, 21) , (250, 244, 0), (255, 158, 72), (255, 69, 161) , (255, 3, 227))]
+((0, 0, 0), (0, 0, 0), (2, 54, 2), (0, 90, 0), (0, 126, 0), (0, 165, 0), (0, 209, 0), (0, 237, 0), (0, 237, 0), (35, 255, 35), (76, 255, 76), (115, 255, 115), (148, 255, 148), (187, 255, 187), (226, 255, 226)),
+((0, 0, 0), (0, 0, 0),(80, 233, 246), (106, 198, 243), (134, 162, 238) , (165, 119, 234), (196, 79, 230), (225, 40, 226) , (248, 10, 223)),
+((0, 0, 0), (0, 0, 0),(80, 233, 246), (76, 234, 232), (65, 237, 199) , (52, 240, 159), (39, 244, 118), (24, 248, 75) , (12, 251, 37), (3, 254, 9)),
+((0, 0, 0), (0, 0, 0),(255, 0, 28), (255, 0, 253), (141, 0, 255) , (0, 26, 255), (0, 255, 238), (0, 255, 33) , (161, 255, 0), (255, 225, 0), (255, 95, 0)),
+((0, 0, 0), (0, 0, 0),(0, 255, 2), (0, 198, 94), (8, 84, 255) , (112, 25, 255), (159, 7, 255), (158, 7, 255) ),
+((0, 0, 0), (0, 0, 0),(246, 249, 3), (247, 226, 3), (249, 192, 0) , (250, 152, 1), (252, 114, 1), (253, 75, 1) , (254, 44, 0), (255, 24, 0)),
+((0, 0, 0), (0, 0, 0),(1, 251, 255), (53, 255, 189), (198, 255, 21) , (250, 244, 0), (255, 158, 72), (255, 69, 161) , (255, 3, 227))]
 
 p=pyaudio.PyAudio()
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
@@ -66,9 +74,9 @@ def Pitch(signal):
 def cycleMode():
     global ramp_index
     global mode_type
-        
+
     if mode_type < 1:
-        mode_type += 1        
+        mode_type += 1
     else:
         mode_type = 0
         #change the ramp
@@ -79,28 +87,40 @@ def cycleMode():
     print "mode: {}", mode_type
     print "ramp index: {}", ramp_index
 
-keyboard.add_hotkey('space', lambda: cycleMode())
+#keyboard.add_hotkey('space', lambda: cycleMode())
 
 while True:
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
 
     #set the number of color ramps we have
     num_colors = len(color_ramps[ramp_index])
-    
+
     #determine the mode and act accordingly
     if amp_freq == 0:
-        
         peak=np.average(np.abs(data))*2
         amp = peak/current_amp
-        print "%f Amplitutde" %peak    
-        #print peak
-        for j in range(num_colors):
-            if amp > j/(float(num_colors)):
-                color = color_ramps[ramp_index][j]
+        print "%f Amplitude" % peak
+
+        # Screensaver
+        if amp < 1/float(num_colors): # It is quiet
+            print ("It is quiet")
+            if time() - sleep_time > NAP_TIME:
+                if not(napping_color): # Assign a random screensaver color 
+                    napping_color = [random.randint(0,20), 0, random.randint(0,20)] # Screensaver color
+                color = napping_color
+        else:
+            sleep_time = time() # Update the time for screensave
+            napping_color = []
+
+            # Assign color based on relative amplitude
+            for j in range(num_colors):
+                if amp > j/(float(num_colors)):
+                    color = color_ramps[ramp_index][j]
+
     elif amp_freq == 1:
         #use the frequency mode
         Frequency=Pitch(data)
-        print "%f Frequency" %Frequency        
+        print "%f Frequency" %  Frequency
         for j in range(num_colors):
             if Frequency/current_freq > j/(float(num_colors)):
                 color = color_ramps[ramp_index][j]
@@ -110,7 +130,7 @@ while True:
     ser1.write("{0},{1},{2},{3},".format(mode_type, color[0], color[1], color[2]))
     #ser2.write("{0},{1},{2},{3},".format(mode_type, color[0], color[1], color[2]))
 
-  
+
 
 
 ser1.close()
